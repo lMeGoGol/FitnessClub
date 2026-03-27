33aaf4382f588a3ca3180ce6b825fd181612db9f
𝓓𝓶𝔂𝓽𝓻𝓸 𝓨𝓪𝓴𝓸𝓿𝓮𝓷𝓴𝓸 <megogo70444@gmail.com>
2026-03-27 15:18:43 +0200

    Добавьте файлы проекта.

---
+++ FitnessClub/Factories/MembershipFactory.cs
@@ -0,0 +1,12 @@
+﻿using FitnessClub.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessClub.Factories
+{
+    internal abstract class MembershipFactory
+    {
+        public abstract IMembership GetMembership();
+    }
+}
